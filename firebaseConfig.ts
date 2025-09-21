79f33032c849dcf547cc6bc642fcdd8b64b7b616
Seungmo Ku <sungmo5845_@naver.com>
2025-09-21 13:21:07 +0900

    Use environment variables for Firebase config
    Replaced hardcoded Firebase configuration values with environment variables to improve security and flexibility.

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -3,14 +3,14 @@ import { initializeApp, getApps, getApp } from 'firebase/app'
 
 
 const firebaseConfig = {
-    apiKey: 'AIzaSyA4hSWNe7NGf3ZjDOx6qEkkEgrXqPin0O4',
-    authDomain: 'jerry-a9e31.firebaseapp.com',
-    projectId: 'jerry-a9e31',
-    storageBucket: 'jerry-a9e31.firebasestorage.app',
-    messagingSenderId: '375283204113',
-    appId: '1:375283204113:web:717bc91a1a271311d631b0',
-    measurementId: 'G-WDTSGF9Y5F'
-}
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID!,
+};
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp()
 
