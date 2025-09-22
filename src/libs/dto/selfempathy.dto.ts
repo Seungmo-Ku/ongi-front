4990a604d0dc23665222225de286afddd939cdd0
Seungmo Ku <sungmo5845_@naver.com>
2025-09-22 23:28:14 +0900

    Update omitted field in SelfEmpathyCreateRequest type
    Changed the omitted field from '_id' to 'id' in the SelfEmpathyCreateRequest type to match the ISelfEmpathy interface.

--- src/libs/dto/selfempathy.dto.ts
+++ src/libs/dto/selfempathy.dto.ts
@@ -1,4 +1,4 @@
 import { ISelfEmpathy } from '@/libs/interfaces/self-empathy.interface'
 
 
-export type SelfEmpathyCreateRequest = Omit<ISelfEmpathy, '_id' | 'createdAt' | 'summary'> & { sid: string }
+export type SelfEmpathyCreateRequest = Omit<ISelfEmpathy, 'id' | 'createdAt' | 'summary'> & { sid: string }
